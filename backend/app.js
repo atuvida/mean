f1bf3481652c1d0c029dcf5fe53559a6ad1c9cff
atuvida <alexandertuvida@gmail.com>
2019-06-05 20:35:12 +0800

    edit

--- backend/app.js
+++ backend/app.js
@@ -30,7 +30,7 @@ app.use((req, res, next) => {
   );
   res.setHeader(
     "Access-Control-Allow-Methods",
-    "GET, POST, PATCH, DELETE, OPTIONS"
+    "GET, POST, PATCH, DELETE, OPTIONS, PUT"
   );
   next();
 });
@@ -48,6 +48,17 @@ app.post("/api/posts", (req, res, next) => {
   });
 });
 
+app.put("/api/posts/:id", (req, res, next) => {
+  const post = new Post({
+    _id: req.body.id,
+    title: req.body.title,
+    content: req.body.content
+  });
+  Post.updateOne({_id: req.params.id}, post).then(result => {
+    res.status(200).json({message: 'Update successful!'});
+  });
+});
+
 app.get("/api/posts", (req, res, next) => {
   Post.find().then(documents => {
     console.log(documents);
@@ -55,6 +66,16 @@ app.get("/api/posts", (req, res, next) => {
       message: "Posts fetched successfully!",
       posts: documents
     });
+  });
+});
+
+app.get("/api/posts/:id", (req, res, next) => {
+  Post.findById(req.params.id).then(post => {
+    if (post) {
+      res.status(200).json({post, message:'post found'});
+    } else {
+      res.status(404).json({message: "Post not found."})
+    }
   });
 });
 
--- src/app/posts/post-create/post-create.component.ts
+++ src/app/posts/post-create/post-create.component.ts
@@ -1,28 +1,58 @@
-import { Component, OnInit} from '@angular/core';
+import { Component, OnInit, AfterViewInit} from '@angular/core';
 import { NgForm } from '@angular/forms';
 import { PostsService } from '../posts.service';
+import { ActivatedRoute, ParamMap } from '@angular/router';
+import { Post } from '../post.model';
+import { stringify } from '@angular/compiler/src/util';
 @Component({
   selector: 'app-post-create',
   templateUrl: './post-create.component.html',
   styleUrls: ['./post-create.component.css']
 })
-export class PostCreateComponent implements OnInit {
+export class PostCreateComponent implements OnInit, AfterViewInit{
 
   enteredTitle = '';
   enteredContent = '';
   postCreated;
+  private mode = 'create';
+  private postId: string;
+  post: Post;
 
-  onAddPost(form: NgForm) {
+  constructor(public postsService: PostsService, public route: ActivatedRoute) { }
+
+  ngAfterViewInit() {
+  }
+
+  ngOnInit() {
+    this.route.paramMap.subscribe((paramMap: ParamMap) => {
+      if (paramMap.has('postId')) {
+        this.mode = 'edit';
+        this.postId = paramMap.get('postId');
+        this.postsService.getAsyncData(this.postId)
+        .then(postData => {
+          this.post = {id: postData.id, title: postData.title, content: postData.content};
+          console.log('inasyncdata' + this.post);
+        });
+        //.subscribe(postData =>  {
+        //   this.post = { id: postData._id, title: postData.title, content: postData.content};
+        // });
+      } else {
+        this.mode = 'create';
+        this.postId = null;
+      }
+    });
+  }
+
+  onSavePost(form: NgForm) {
     if (form.invalid) {
       return;
     }
-    this.postsService.addPost(form.value.title, form.value.content);
+    console.log(this.mode);
+    if (this.mode === 'create') {
+      this.postsService.addPost(form.value.title, form.value.content);
+    } else {
+      this.postsService.updatePost(this.postId, form.value.title, form.value.content);
+    }
     form.resetForm();
   }
-
-  constructor(public postsService: PostsService) { }
-
-  ngOnInit() {
-  }
-
 }
